bb01af4ac6fcdc1d7daa48f43d8d649cf74f0189
Miguel Cabrera Noguera <mcabreran6@uoc.edu>
2025-11-06 18:33:39 +0100

    Added JS code for fading buttons and links. Pages modified. CSS style modified

--- src/mainScript.js
+++ src/mainScript.js
@@ -7,7 +7,36 @@ import 'lightgallery/css/lg-thumbnail.css';
 import lgZoom from 'lightgallery/plugins/zoom';
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 
-lightGallery(document.getElementById('gallery'), {
-    plugins: [lgZoom, lgThumbnail],
-    speed: 500,
+document.addEventListener("DOMContentLoaded", () => {
+
+    // LightGallery module
+
+    const gallery = document.getElementById("gallery");
+
+    if(gallery){
+        try{
+            lightGallery(document.getElementById('gallery'), {
+                plugins: [lgZoom, lgThumbnail],
+                speed: 500,
+            });
+        } catch (e){
+            console.error("Error al cargar LightGallery");
+        }
+    }
+
+    //buttons and links JS
+
+    const fades = document.querySelectorAll(".fade");
+
+    fades.forEach(f => {
+        f.addEventListener("click", (e) => {
+            e.preventDefault();
+            document.body.classList.add("fade-out");
+
+            setTimeout(() => {
+                window.location.href = f.href;
+            }, 500);
+        });
+    });
 });
+
